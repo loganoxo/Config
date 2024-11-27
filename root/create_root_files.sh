ea89035006e61c99e20ed6730bfdfe0e9fbbca8d
loganoxo@163.com <loganoxo@163.com>
2024-11-28 01:29:58 +0800

    bashrc bash_profile 合并

--- root/create_root_files.sh
+++ root/create_root_files.sh
@@ -56,7 +56,7 @@ files=(".bashrc" ".bash_profile" ".zshrc" ".zprofile")
 # 备份
 timestamp=$(date +%Y%m%d%H%M%S)
 backup_dir="$target_dir/.shell_bak/$timestamp/"
-mkdir "$backup_dir"
+mkdir -p "$backup_dir"
 for file in "${files[@]}"; do
     original_file="$target_dir/$file"
     if [ -f "$original_file" ]; then

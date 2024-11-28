94d8f159a8b0b177b407eb65a776bb83fc533195
loganoxo@163.com <loganoxo@163.com>
2024-11-29 01:28:24 +0800

    zk.sh

--- shell/zk.sh
+++ shell/zk.sh
@@ -2,8 +2,10 @@
 # This is controller for start/stop-zk-all.sh .
 set -e
 
+# https://dlcdn.apache.org/zookeeper/zookeeper-3.8.4/apache-zookeeper-3.8.4-bin.tar.gz
 ################### local ######################
-local_zk_home='/Users/logan/Data/Software/zookeeper3.8.0'
+local_zk_home='/Users/logan/Data/Software/zookeeper'
+#localDirArray=()
 localDirArray=(
     "cluster/zookeeper1"
     "cluster/zookeeper2"
@@ -14,20 +16,24 @@ localDirArray=(
 # remote ip
 remoteHosts=("1,1,1,1")
 # remote dir
-remoteDir="/usr/local/software/zookeeper-3.8.0"
+remoteDir="/usr/local/software/zookeeper"
 
 ################## function ###################
 startRemote() {
+    echo -e "\033[31m  启动远程的  \033[0m"
     index=1
     for host in "${remoteHosts[@]}"; do
         echo -e "\033[31m Zookeeper$index start on $host... \033[0m"
+        # 不用管, $remoteDir 就是要在客户端解析
+        # shellcheck disable=SC2029
         ssh "root@$host" "source /etc/profile; $remoteDir/bin/zkServer.sh start"
         echo -e "\n"
         index=$((index + 1))
-        sleep 1s
+        sleep 1
     done
 }
 startLocal() {
+    echo -e "\033[31m  启动本地的  \033[0m"
     index=1
     for dir in "${localDirArray[@]}"; do
         absolute_dir="$local_zk_home/$dir"
@@ -35,21 +41,25 @@ startLocal() {
         "$absolute_dir/bin/zkServer.sh" start
         echo -e "\n"
         index=$((index + 1))
-        sleep 1s
+        sleep 1
     done
 }
 
 stopRemote() {
+    echo -e "\033[31m  终止远程的  \033[0m"
     index=1
     for host in "${remoteHosts[@]}"; do
         echo -e "\033[31m Zookeeper$index stop on $host... \033[0m"
+        # 不用管, $remoteDir 就是要在客户端解析
+        # shellcheck disable=SC2029
         ssh "root@$host" "source /etc/profile; $remoteDir/bin/zkServer.sh stop"
         echo -e "\n"
         index=$((index + 1))
-        sleep 1s
+        sleep 1
     done
 }
 stopLocal() {
+    echo -e "\033[31m  终止本地的  \033[0m"
     index=1
     for dir in "${localDirArray[@]}"; do
         absolute_dir="$local_zk_home/$dir"
@@ -57,21 +67,25 @@ stopLocal() {
         "$absolute_dir/bin/zkServer.sh" stop
         echo -e "\n"
         index=$((index + 1))
-        sleep 1s
+        sleep 1
     done
 }
 
 checkRemote() {
+    echo -e "\033[31m  检查远程的  \033[0m"
     index=1
     for host in "${remoteHosts[@]}"; do
         echo -e "\033[31m Zookeeper$index check on $host... \033[0m"
+        # 不用管, $remoteDir 就是要在客户端解析
+        # shellcheck disable=SC2029
         ssh "root@$host" "source /etc/profile; $remoteDir/bin/zkServer.sh status"
         echo -e "\n"
         index=$((index + 1))
-        sleep 1s
+        sleep 1
     done
 }
 checkLocal() {
+    echo -e "\033[31m  检查本地的  \033[0m"
     index=1
     for dir in "${localDirArray[@]}"; do
         absolute_dir="$local_zk_home/$dir"
@@ -79,7 +93,7 @@ checkLocal() {
         "$absolute_dir/bin/zkServer.sh" status
         echo -e "\n"
         index=$((index + 1))
-        sleep 1s
+        sleep 1
     done
 }
 

7cbf0aef6276748ac3f60f81067c7c65ad453e72
loganoxo@163.com <loganoxo@163.com>
2024-12-01 01:36:40 +0800

    nginx

---
+++ nginx/nginx-ln.sh
@@ -0,0 +1,4 @@
+export __PATH_MY_CNF="$HOME/Data/Config" # 我自己的配置文件目录
+
+ln -sf "${__PATH_MY_CNF}/nginx/www" "/opt/homebrew/var/www"
+ln -sf "${__PATH_MY_CNF}/nginx/etc/nginx.conf" "/opt/homebrew/etc/nginx/nginx.conf"

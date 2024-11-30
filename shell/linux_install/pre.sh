afb7cd5c34c8e58e6e86c43fc2b0cd774e8fb1be
loganoxo@163.com <loganoxo@163.com>
2024-12-01 01:38:34 +0800

    nginx

--- shell/linux_install/pre.sh
+++ shell/linux_install/pre.sh
@@ -6,6 +6,7 @@
 # 描述: linux装机前置脚本; 包括软件源的配置、网络静态IP和DNS等
 # 使用: 1.用wget(debian默认安装)        su -c 'wget -O- https://raw.githubusercontent.com/loganoxo/Config/master/shell/linux_install/pre.sh | bash'
 # 2.用curl(debian等linux可能没有预装)   su -c 'curl -fsSL https://raw.githubusercontent.com/loganoxo/Config/master/shell/linux_install/pre.sh | bash'
+# 也可以放在nginx中
 
 set -eu #e:遇到错误就停止执行；u:遇到不存在的变量，报错停止执行
 

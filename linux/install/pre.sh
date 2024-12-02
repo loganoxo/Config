ad3b1eef84500c5bb62689fab914ac57d918802f
loganoxo@163.com <loganoxo@163.com>
2024-12-02 17:06:12 +0800

    linux

--- linux/install/pre.sh
+++ linux/install/pre.sh
@@ -7,6 +7,7 @@
 # 使用: 1.用wget(debian默认安装)        su -c 'wget -q -O- https://raw.githubusercontent.com/loganoxo/Config/master/linux/install/pre.sh | bash -s -- run'
 # 2.用curl(debian等linux可能没有预装)   su -c 'curl -fsSL https://raw.githubusercontent.com/loganoxo/Config/master/linux/install/pre.sh | bash'
 # 也可以放在nginx中
+# 提示信息不能使用中文,因为linux自己的tty终端不支持中文
 
 set -eu #e:遇到错误就停止执行；u:遇到不存在的变量，报错停止执行
 flag="$1"
@@ -29,22 +30,22 @@ function _logan_if_linux() {
 
 function judge() {
     if [ -z "$flag" ]; then
-        echo "需要参数"
+        echo "need arg"
         exit 1
     fi
 
     if [ "$flag" != 'run' ]; then
-        echo "参数应该是 run "
+        echo "arg must be 'run' "
         exit 1
     fi
 
     if _logan_if_mac; then
-        echo "mac 不能执行"
+        echo "can't run on macos"
         exit 1
     fi
 
     if ! _logan_if_linux; then
-        echo "当前非linux系统, 不能执行"
+        echo "only support linux"
         exit 1
     fi
 }
@@ -54,22 +55,22 @@ judge
 
 # 软件源配置
 function software_config() {
-    echo -e "\033[31m 当前软件源配置为:  \033[0m \n"
+    echo -e "\033[31m current sources :  \033[0m \n"
     cat /etc/apt/sources.list
-    echo -n "确定要重新配置吗? (y/n): "
+    echo -n "Are you sure you want to reconfigure? (y/n): "
     read -r choice1 < /dev/tty
     case "$choice1" in
     y | Y) ;;
     *)
-        echo "操作取消.脚本停止"
+        echo "Operation cancelled. Script stopped."
         exit 1 #脚本停止
         ;;
     esac
 
     local version=""
-    str0="\033[31m 配置官方软件源: 选择 debian 的版本  \033[0m \n"
-    str1="\033[31m[ 1 ] debian11(bullseye) \033[0m \n"
-    str2="\033[31m[ 2 ] debian12(bookworm) \033[0m \n"
+    str0="\033[31m Configure official software sources: Choose the Debian version  \033[0m \n"
+    str1="\033[31m[ 1 ] debian11 (bullseye) \033[0m \n"
+    str2="\033[31m[ 2 ] debian12 (bookworm) \033[0m \n"
     str3="\033[31m[ 3 ] Exit. \033[0m \n"
     echo -e "$str0$str1$str2$str3"
     echo -n "Input your choice :"
@@ -78,7 +79,7 @@ function software_config() {
     1) version="bullseye" ;;
     2) version="bookworm" ;;
     *)
-        echo "操作取消.脚本停止"
+        echo "Operation cancelled. Script stopped."
         exit 1 #脚本停止
         ;;
     esac

329e3b49c90463721ec9220021f965d0292660f2
loganoxo@163.com <loganoxo@163.com>
2024-12-07 06:19:49 +0800

    logan_check

--- shell/logan_check.sh
+++ shell/logan_check.sh
@@ -18,7 +18,7 @@ repos=(
 for repo in "${repos[@]}"; do
     if_clean=1
     if_has_remote=1
-    echo -e "\033[31mChecking $repo \033[0m......"
+    echo -e "\033[32mChecking $repo \033[0m......"
     cd "$repo" || {
         echo -e "    \033[31m Failed to access \033[0m"
         continue
@@ -35,13 +35,13 @@ for repo in "${repos[@]}"; do
     untracked=$(git status --porcelain | grep -c '^??') || true
     if [ "$untracked" -gt 0 ]; then
         if_clean=0
-        echo -e "    \033[31m UnTracked files:\033[0m $untracked"
+        echo -e "    \033[35m UnTracked files:\033[0m $untracked"
     fi
 
     modified=$(git status --porcelain | grep -c '^[[:space:]]*[MADRC]') || true
     if [ "$modified" -gt 0 ]; then
         if_clean=0
-        echo -e "    \033[31m UnCommited files:\033[0m $modified"
+        echo -e "    \033[35m UnCommited files:\033[0m $modified"
     fi
 
     # 如果有远程仓库
@@ -50,19 +50,19 @@ for repo in "${repos[@]}"; do
         ahead=$(git rev-list --count @{upstream}..HEAD 2>/dev/null) || true
         if [ "$ahead" -gt 0 ]; then
             if_clean=0
-            echo -e "    \033[31m UnPushed commits:\033[0m $ahead"
+            echo -e "    \033[35m UnPushed commits:\033[0m $ahead"
         fi
 
         # 检测本地分支落后远程的情况
         behind=$(git rev-list --count HEAD..@{upstream} 2>/dev/null) || true
         if [ "$behind" -gt 0 ]; then
             if_clean=0
-            echo -e "    \033[31m Local branch is behind upstream by\033[0m $behind\033[31m commits\033[0m"
+            echo -e "    \033[35m Local branch is behind upstream by\033[0m $behind\033[31m commits\033[0m"
         fi
     fi
 
     if [ "$if_clean" -eq 1 ]; then
-        echo -e "    \033[31m everything is clean \033[0m"
+        echo -e "    \033[33m everything is clean \033[0m"
     fi
     echo ""
     echo ""

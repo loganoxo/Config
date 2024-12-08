ace700fba912f9de270b4728f19668ee3c1b0d80
loganoxo@163.com <loganoxo@163.com>
2024-12-08 19:18:25 +0800

    share 文件夹 改为 Share

--- nginx/nginx-ln.sh
+++ nginx/nginx-ln.sh
@@ -1,5 +1,5 @@
 export __PATH_MY_CNF="$HOME/Data/Config" # 我自己的配置文件目录
-
+# 可能需要先删除 "/opt/homebrew/var/www" 目录
 ln -sf "${__PATH_MY_CNF}/nginx/www" "/opt/homebrew/var/www"
 ln -sf "$HOME/Share/nginx" "${__PATH_MY_CNF}/nginx/www/files/share"
 ln -sf "${__PATH_MY_CNF}/nginx/etc/nginx.conf" "/opt/homebrew/etc/nginx/nginx.conf"

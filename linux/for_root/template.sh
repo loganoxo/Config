29085facbaf54667ad560707be23b06583b5799b
loganoxo@163.com <loganoxo@163.com>
2024-12-10 00:59:37 +0800

    zshenv

--- linux/for_root/template.sh
+++ linux/for_root/template.sh
@@ -6,8 +6,22 @@ export __PATH_MY_SOFT_FOR_ROOT="$_common_home_for_root/Data/Software"           
 export __PATH_MY_CNF_SENSITIVE_FOR_ROOT="$_common_home_for_root/Data/ConfigSensitive" # 本地敏感数据目录
 export __PATH_HOME_CONFIG_FOR_ROOT="$_common_home_for_root/.config"                   # 默认配置目录
 
-export PATH=$PATH:/usr/sbin/       # 针对linux系统的默认PATH没有sbin的问题
-export PATH=$PATH:$HOME/.local/bin # 针对linux系统
+# set PATH so it includes user's private bin if it exists
+if [ -d "$HOME/bin" ]; then
+    export PATH="$HOME/bin:$PATH"
+fi
+
+# 针对uv等工具
+if [ -d "$HOME/.local/bin" ]; then
+    export PATH="$HOME/.local/bin:$PATH"
+fi
+
+# 针对linux系统的默认PATH没有sbin的问题
+if [ -d "/usr/sbin" ]; then
+    export PATH="$PATH:/usr/sbin"
+fi
+
+##########################################################################################
 
 export TERM=xterm-256color
 # 加载通用函数

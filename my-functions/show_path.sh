e089f6a8daabe64af5ef789ca0dd066dde156a6b
loganoxo@163.com <loganoxo@163.com>
2024-12-12 02:03:27 +0800

    show_path.sh

--- my-functions/show_path.sh
+++ my-functions/show_path.sh
@@ -6,3 +6,12 @@ function show_path() {
     count=$(echo "$PATH" | tr ':' '\n' | wc -l)
     echo "总共有 $count 个路径。"
 }
+
+function show_unique_path() {
+    echo "PATH 中的各个路径为(去重)："
+    foo="$(bash "${__PATH_MY_CNF}/shell/clean_path.sh")"
+    echo "$foo" | tr ':' '\n'
+    # 统计路径的数量
+    count=$(echo "$foo" | tr ':' '\n' | wc -l)
+    echo "总共有 $count 个路径。"
+}

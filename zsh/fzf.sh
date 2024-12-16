9e512fbb57811c4a753bdec8f9f531136002a5cb
loganoxo@163.com <loganoxo@163.com>
2024-12-17 02:15:05 +0800

    tmux

--- zsh/fzf.sh
+++ zsh/fzf.sh
@@ -113,3 +113,16 @@ zf() {
     )
     [ -d "$dir" ] && cd "$dir" || return
 }
+
+function fkill() {
+  local pid
+  if [ "$UID" != "0" ]; then
+    pid=$(ps -f -u $UID | sed 1d | (fzf -m --preview='') | awk '{print $2}')
+  else
+    pid=$(ps -ef | sed 1d | (fzf -m --preview='') | awk '{print $2}')
+  fi
+
+  if [ "x$pid" != "x" ]; then
+    echo "$pid" | xargs kill -"${1:-9}"
+  fi
+}
